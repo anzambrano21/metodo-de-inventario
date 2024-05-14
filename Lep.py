03b27cc5bc8bb3e220478a7b5df741cd5d2489b1
anzambrano21 <lolepico1234@gmail.com>
2024-05-14 13:08:25 -0400

    creacion de lep con faltantes

--- Lep.py
+++ Lep.py
@@ -7,12 +7,24 @@ class Lepp:
         self.td=0
         self.c
     def costo(self,costoP,costoOp,costoMi):
-        q=sqrt((2*self.td*costoOp)/(costoMi*(1-self.td/self.Tp)))
+        q=self.cantidadInv(costoOp,costoMi)
         self.c=costoP*q+costoOp+costoMi*(pow(q,2)*(1-self.td/self.Tp))/self.td*2
+        return self.c
     #tasa de produccion
     def setTp(self,Tp):
         self.Tp=Tp
     #tasa de demanda
     def setTp(self,td):
         self.td=td
+    def cantidadInv(self,costoOp,costoMi):
+        return sqrt((2*self.td*costoOp)/(costoMi*(1-self.td/self.Tp)))
+class LeppFal(Lepp):
+    def __init__(self):
+        super().__init__()
+    def costo(self,costoP,costoOp,costoMi,cf):
+        q=super().cantidadInv(costoOp,costoMi)*sqrt((costoMi+cf)/cf)
+        s=sqrt(2*self.td*costoOp/cf)*sqrt(1-self.td/self.Tp)*sqrt(costoMi/(costoMi+cf))
+        self.c=costoOp*q+costoOp+(costoMi/2)*pow(q*(1-self.td/self.Tp)-s,2)*(1/(self.Tp-self.Tp)+1/self.td)+(cf*s*s/2)*(1/(self.Tp-self.Tp)+1/self.td)
+        return self.c
+
 
