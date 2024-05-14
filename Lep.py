a2259de4025dd0ef209daada00f7fdebebe4a7d7
anzambrano21 <lolepico1234@gmail.com>
2024-05-14 11:21:01 -0400

    creacion de lep

---
+++ Lep.py
@@ -0,0 +1,15 @@
+""" Q cantidad de inventario D  es la demanda R tasa de produccioon """
+
+class Lepp:
+    
+    def __init__(self):
+        self.Tp=0
+        self.td=0
+        self.c
+    def costo(self,costoP,costoOp,costoMi):
+        self.c
+    def setTp(self,Tp):
+        self.Tp=Tp
+    def setTp(self,td):
+        self.td=td
+
